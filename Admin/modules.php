4f33112ae42234de37cd51d17f5f333a811166f2
sujanghi129-pixel <sujanghi129@gmail.com>
2026-03-26 19:09:09 +0100

    altered nav bar

--- Admin/modules.php
+++ Admin/modules.php
@@ -203,6 +203,16 @@ $studentCount = $r ? $r->fetch_assoc()['c'] : 0;
                             Programmes
                         </a>
                     </li>
+                   <li>
+                        <a href="staff.php" class="sidebar-link">
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
+                                <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />
+                            </svg>
+                            👨‍🏫 Staff
+                        </a>
+                    </li>
                     <li>
                         <a href="create_admin.php" class="sidebar-link">
                             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor"
