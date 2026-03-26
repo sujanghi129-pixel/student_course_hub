6693622d14d2fb5bfbd9b86f0c18596f370f75d0
2105raj <rajani.shrestha34@swissedu.co.uk>
2026-03-26 17:57:25 +0100

    changed nav bar

--- Admin/students.php
+++ Admin/students.php
@@ -252,7 +252,7 @@ $deleted    = isset($_GET['deleted']);
                     <path d="M6 12v5c3 3 9 3 12 0v-5"/>
                 </svg>
             </div>
-            <span>Course<strong>Hub</strong></span>
+            <span>Niels<strong> Brock University</strong></span>
         </div>
 
         <p class="sidebar-section-label">Navigation</p>
@@ -277,6 +277,12 @@ $deleted    = isset($_GET['deleted']);
                     <a href="programmes.php" class="sidebar-link">
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/></svg>
                         Programmes
+                    </a>
+                </li>
+                <li>
+                    <a href="create_admin.php" class="sidebar-link">
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/></svg>
+                        ➕ Create User
                     </a>
                 </li>
                 <li>
